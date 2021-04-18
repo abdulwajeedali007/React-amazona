c01db7096a246a828502d3dbb79844a981e25106
Wajeed <abdulwajeedali007@gmail.com>
2021-04-18 15:21:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./index.css";
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+         <a href="#">ReactShoppingCART</a>
+      </header>
+      <main>Product List</main>
+      <footer>
+         this is footer
+      </footer>
+    </div>
+  )
+}
+
+export default App
+
