8ef2873dcac047570f8fbcc638103e4709b6936d
Wajeed <abdulwajeedali007@gmail.com>
2021-04-19 20:25:06 +0530

    Chart Component

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,7 +2,38 @@ import React, { Component } from 'react'
 import formatCurrency from '../Util'
 
 export default class Cart extends Component {
+    constructor(props) {
+        super(props)
     
+        this.state = {
+             isOpen : false,
+             name: '',
+             email: '',
+             address: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange = (e) => {
+      this.setState({[e.target.name]: e.target.value})
+    }
+    
+
+    handleSubmitdata = (e) => {
+        e.preventDefault();
+        const  order = {
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            cartItems: this.props.cartItems,
+        }
+
+        this.props.handleSubmitdata(order)
+
+    }     
+
+
     render() {
         const {cartItems, removeFromCart} = this.props;
         return (
@@ -31,10 +62,33 @@ export default class Cart extends Component {
                         <h3>Total :</h3>
                         <h2>{formatCurrency(cartItems.reduce((a,c)=> a + c.price * c.count, 0))}</h2>
                     </div>
-                    <button className="proceed">Proceed</button>
+                    <button className="proceed" onClick={()=>this.setState({isOpen: true})}>Proceed</button>
                     </div>
                 ) }
-
+               {this.state.isOpen && (
+                   <div className="cartForm">
+                      <h4>Enter Address</h4>
+                      <hr/>
+                      <form onSubmit={this.handleSubmitdata}>
+                        <div className="form_box">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} className="email inputBox" id="email" required/>
+                        </div>
+                        <div className="form_box">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} className="name inputBox" id="name" required/>
+                        </div>
+                        <div className="form_box">
+                            <label htmlFor="Address">Address</label>
+                            <input type="text" name="address" value={this.state.address} onChange={this.handleChange} className="address inputBox" id="Address" required/>
+                        </div>
+                        <br/>
+                        <div className="form_box">
+                           <button type="submit" className="button btn-submit primary">Submit</button>
+                        </div>
+                      </form>
+                   </div>
+               )}
             </div>
         )
     }
